96a8b68b20a669a5388c91b734e6ad707d61052a
maksim.vasilevski <max31051980@hotmail.com>
2018-02-19 23:22:36 +0300

    add new string Hello World from master branch again

--- hw.java
+++ hw.java
@@ -3,6 +3,6 @@ public class hw {
         System.out.println("Hello World!");
         System.out.println("Hello World from master branch!");
         System.out.println("Hello World from dev branch!");
-
+        System.out.println("Hello World from master branch again!");
     }
 }
